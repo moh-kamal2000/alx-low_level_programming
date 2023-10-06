1b769c4f305ad91d38229af3ef0996cc6d90f1f4
moh-kamal2000 <fofo.moh20000@gmail.com>
2023-10-05 19:04:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
-
-/**
- * main -entry point 
- * description : A C program that prints with put function 
- *
- * return always 0 (success)
-*/
-
-int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle");
 	return (0);
-
 }
