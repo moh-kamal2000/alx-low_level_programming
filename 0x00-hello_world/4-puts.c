bd05eb1457792957c50e60aeee61be7a6a81e830
moh-kamal2000 <fofo.moh20000@gmail.com>
2023-10-05 18:45:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/*
+ * main 
+ * descripptiton 
+ * return 0 
+ *\
+ 
+#include <stdio.h>
+int main (){
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+
+}
