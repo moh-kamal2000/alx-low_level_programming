c264bd5e5b45d65c7f3d6caea93bb5d35acc2a4e
moh-kamal2000 <fofo.moh20000@gmail.com>
2023-10-05 18:59:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
  * return always 0 (success)
 */
 
-int main (){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle");
+return (0);
 
 }
